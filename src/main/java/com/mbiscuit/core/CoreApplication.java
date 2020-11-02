2ec656656b5197ab12175e9c3b17d81f620dccb9
wangxiaolei <wangxiaolei@odianyun.com>
2020-11-02 19:31:13 +0800

    初始化项目

--- src/main/java/com/mbiscuit/core/CoreApplication.java
+++ src/main/java/com/mbiscuit/core/CoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoreApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.config.additional-location", "file:${user.home}/mbiscuit/");
         SpringApplication.run(CoreApplication.class, args);
     }
 
