fb06c25bec93e3e4922a0efce7624fa57224e378
wangxiaolei <wangxiaolei@odianyun.com>
2020-11-10 16:30:32 +0800

    添加xxl

--- common/src/main/java/com/mbiscuit/core/common/config/XxlConfig.java
+++ common/src/main/java/com/mbiscuit/core/common/config/XxlConfig.java
@@ -1,17 +1,21 @@
 package com.mbiscuit.core.common.config;
 
 import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class XxlConfig {
 
+    @Value("${xxl.admin-address}")
+    private String adminAddress;
+
     @Bean(initMethod = "start", destroyMethod = "destroy")
     public XxlJobSpringExecutor xxlJobExecutor(){
         XxlJobSpringExecutor xxlJobExecutor = new XxlJobSpringExecutor();
-        xxlJobExecutor.setAdminAddresses("http://localhost:8082/xxl-job-admin");
-        xxlJobExecutor.setAppName("mbiscuit");
+        xxlJobExecutor.setAdminAddresses(adminAddress);
+        xxlJobExecutor.setAppname("mbiscuit");
         return xxlJobExecutor;
     }
 }
